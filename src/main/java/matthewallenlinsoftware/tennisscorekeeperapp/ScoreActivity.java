073373b9597107bf70127b96ccf39417fa67f771
Darthpwner <matthewallenlin@gmail.com>
2017-07-15 17:25:17 -0700

    Fixed up P2 label on small screens

--- src/main/java/matthewallenlinsoftware/tennisscorekeeperapp/ScoreActivity.java
+++ src/main/java/matthewallenlinsoftware/tennisscorekeeperapp/ScoreActivity.java
@@ -36,6 +36,7 @@ public class ScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
